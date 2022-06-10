6336dc3fdd955c06f2b72b59757b3366580b5ddc
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-06-10 15:39:10 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // import ApiService from './js/api.js';
 
 // const apiData = new ApiService();
-// console.log(apiData)
-import getData from './js/api';
-console.log(getData())
+import  getData  from './js/api';
+
+getData().then((base) => console.log(base));
+
