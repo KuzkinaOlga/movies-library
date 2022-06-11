bc1013d189fb445c7cc4f14d82974592fb335703
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-06-11 20:52:33 +0300

    1

--- src/js/render-list.js
+++ src/js/render-list.js
@@ -17,21 +17,21 @@ export function renderList(hits, container) {
         //    }   ) 
         // })
         // console.log(genre_ids.forEach((item)=>console.log(item)))
-   return `<li class="movie-card" data-id=${id}>
-            <a href="" class="movie-link">
-              
-              <picture class="poster-thumb">
+   return  `<a class="post" data-id=${id}>
+  <div class="photo-card">
+  <div class="thumb">
+  <picture class="poster-thumb">
                   
         <source class="lzy_img" media="(min-width: 1200px)" 
-        srcset=""  type="image/jpeg" width="310" height="450"  data-src="https://image.tmdb.org/t/p/w500${poster_path} 1x,https://image.tmdb.org/t/p/w500${poster_path} 2x">
+        srcset=""  type="image/jpeg" width="310" height="450"  data-src="${poster_path} 1x,${poster_path} 2x">
                                                        
       
         <source class="lzy_img" media="(min-width: 768px)"
-         srcset=""  type="image/jpeg" width="335" height="455"  data-src= "https://image.tmdb.org/t/p/w500${poster_path} 1x,https://image.tmdb.org/t/p/w500${poster_path} 2x">
+         srcset=""  type="image/jpeg" width="335" height="455"  data-src= "${poster_path} 1x,${poster_path} 2x">
                                                                     
                                             
         <source class="lzy_img" media="(max-width: 767px)"
-         srcset=""  type="image/jpeg" width="280" height="400"  data-src= "https://image.tmdb.org/t/p/w500${poster_path} 1x,https://image.tmdb.org/t/p/w500${poster_path} 2x">
+         srcset=""  type="image/jpeg" width="280" height="400"  data-src= "${poster_path} 1x,${poster_path} 2x">
                                                
                     <img
                         src="https://image.tmdb.org/t/p/w500${poster_path}"
@@ -40,6 +40,8 @@ export function renderList(hits, container) {
                         class="poster" 
                     />
                 </picture>
+  <div class="info">
+   
                
                 <div class="movie-info">     
                     <h2 class="movie-title">${original_title}</h2>
@@ -50,8 +52,11 @@ export function renderList(hits, container) {
                       <p class="movie-date">${release_date}</p>
                     </div>                                 
                 </div>
-            </a>
-         </li>`
+  </div>
+  </div>
+</div>
+</a>
+        `
 
       }).join('');
       container.insertAdjacentHTML("beforeend", markup);
