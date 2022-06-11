a88df35653177966188261944d43f4b9e6a142ec
ViacheslavDedov <dedoff.v@gmail.com>
2022-06-11 22:35:43 +0300

    додав стилі

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import { renderList } from './js/render-list';
 import { getRefs } from './js/get-refs';
+import  getData  from './js/api';
+
+const refs = getRefs();
+
+
+
 
 // import ApiService from './js/api.js';
 
+
+
+
 // const apiData = new ApiService();
-import  getData  from './js/api';
+
 
 getData().then((base) => console.log(base));
 
