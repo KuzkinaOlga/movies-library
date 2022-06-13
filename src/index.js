3ea8778a98ee7c9f8c1431f09aa9ae30f5f43278
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-06-13 20:14:29 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,29 +1,18 @@
 import { renderList } from './js/render-list';
 import { getRefs } from './js/get-refs';
-
-
-
-
-// const apiData = new ApiService();
-
 import darkTheme from './js/dark-theme';
 import { onShowMyLibrary, onShowHome } from './js/header';
 import './js/pagination';
 import ApiService from './js/api';
-
 import { containerTui } from './js/pagination';
 import { paginationTotalItems } from './js/pagination';
-
+import getTopMovies from './js/api';
 import { onContainerClick } from './js/modal-movie';
 
 
-export let searchBy = '';
-export let queryForTui = '';
 const apiData = new ApiService();
 const container = getRefs().gallery;
 apiData.getGanres()
-
-
 
 darkTheme();
 
@@ -34,21 +23,40 @@ function topMoviesRender() {
 } )}
 topMoviesRender();
 
+// Search movies
+function onFormSubmit(e) {
+    e.preventDefault();
+    apiData.query = e.currentTarget.elements.searchQuery.value.trim();
+    queryForTui = apiData.query;
+    if (!apiData.query) {
+      alert('Please enter name movie');
+      return;
+    } else {
+       apiData.getSearchMovies(apiData.query).then(({results, total_results}) => {
+         if (total_results > 20) {
+        paginationTotalItems(total_results);
+        containerTui.classList.remove('visually-hidden');
+      } 
+      if (results.length === 0) {
+         alert('not find');
+      }else {
+      container.innerHTML = '';
+      renderList(results, container);
+      // searchBy = 'search';
+      }
+    });
+    getRefs().form.reset();
+    }
 
-import getTopMovies from './js/api';
+   
+}
 
-getTopMovies().then(({ results }) => {
-  console.log(results);
-  renderList(results, container);
-});
-
-let searchQuery = '';
 
 // Listiners
 getRefs().logo.addEventListener('click', onLogoClick);
 getRefs().homeBtn.addEventListener('click', onHomeBtnClick);
 getRefs().myLibraryBtn.addEventListener('click', onMyLybraryBtnClick);
-// getRefs().form.addEventListener('submit', onFormSubmit);
+getRefs().form.addEventListener('submit', onFormSubmit);
 getRefs().watchedBtn.addEventListener('click', onWatchedBtnClick);
 getRefs().queueBtn.addEventListener('click', onQueueBtnClick);
 
@@ -57,31 +65,21 @@ function onLogoClick(e) {
   e.preventDefault();
   console.log('onLogoClick');
   onShowHome();
+  topMoviesRender();
 }
 
 function onHomeBtnClick(e) {
   e.preventDefault();
-  console.log('onHomeBtnClick');
   onShowHome();
+  topMoviesRender();
 }
 
 function onMyLybraryBtnClick(e) {
   e.preventDefault();
-  console.log('onMyLybraryBtnClick');
   onShowMyLibrary();
+  container.innerHTML = "";
 }
 
-// function onFormSubmit(e) {
-//   e.preventDefault();
-//   searchQuery = e.currentTarget.elements.searchQuery.value.trim();
-
-//   if (!searchQuery) {
-//     alert('Not correct search key');
-//     return;
-//   }
-//   console.log('onFormSubmit');
-//   console.log(searchQuery);
-// }
 
 function onWatchedBtnClick() {
   if (getRefs().queueBtn.classList.contains('active-btn')) {
@@ -93,84 +91,3 @@ function onWatchedBtnClick() {
 function onQueueBtnClick() {
   if (getRefs().watchedBtn.classList.contains('active-btn')) {
     getRefs().watchedBtn.classList.remove('active-btn');
-
-// Search movies
-function onFormSubmit(e) {
-    e.preventDefault();
-    apiData.query = e.currentTarget.elements.searchQuery.value.trim();
-    queryForTui = apiData.query;
-    if (!apiData.query) {
-      alert('Please enter name movie');
-      return;
-    }
-
-    apiData.getSearchMovies(apiData.query).then(({results, total_results}) => {
-      if (results.length === 0) {
-         alert('not find');
-      } else if (total_results > 20) {
-        paginationTotalItems(total_results);
-        containerTui.classList.remove('visually-hidden');
-      }
-      container.innerHTML = '';
-      renderList(results, container);
-      searchBy = 'search';
-    });
-    console.log('onFormSubmit');
-    console.log(apiData.query);
-    getRefs().form.reset();
-}
-
-
-
-
-  // Listiners
-  getRefs().logo.addEventListener('click', onLogoClick);
-  getRefs().homeBtn.addEventListener('click', onHomeBtnClick);
-  getRefs().myLibraryBtn.addEventListener('click', onMyLybraryBtnClick);
-  getRefs().form.addEventListener('submit', onFormSubmit);
-  getRefs().watchedBtn.addEventListener('click', onWatchedBtnClick);
-  getRefs().queueBtn.addEventListener('click', onQueueBtnClick);
-
-  // Header functions
-  function onLogoClick(e) {
-    e.preventDefault();
-    console.log('onLogoClick');
-    onShowHome();
-    topMoviesRender ()
-  }
-
-  function onHomeBtnClick(e) {
-    e.preventDefault();
-    console.log('onHomeBtnClick');
-    onShowHome();
-    topMoviesRender ()
-  }
-
-  function onMyLybraryBtnClick(e) {
-    e.preventDefault();
-    console.log('onMyLybraryBtnClick');
-    onShowMyLibrary();
-    container.innerHTML = '';
-  }
-
-
-
-  function onWatchedBtnClick() {
-    if (getRefs().queueBtn.classList.contains('active-btn')) {
-        getRefs().queueBtn.classList.remove('active-btn');
-    }
-    getRefs().watchedBtn.classList.add('active-btn');
-
-  }
-
-
-  function onQueueBtnClick() {
-    if (getRefs().watchedBtn.classList.contains('active-btn')) {
-        getRefs().watchedBtn.classList.remove('active-btn');
-    }
-    getRefs().queueBtn.classList.add('active-btn');
-  }
-
-  getRefs().queueBtn.classList.add('active-btn');
-}
-
