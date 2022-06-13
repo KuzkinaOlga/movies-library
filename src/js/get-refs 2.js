8d0e0ad7af16771af1b0e82faf76bbf3332aa03a
KuzkinaOlga <o.kuzkina@gmail.com>
2022-06-13 18:58:11 +0200

    fixed stiles footer

--- src/js/get-refs 2.js
+++
@@ -1,7 +0,0 @@
-export function getRefs() {
-    return {
-        containerMovies: document.querySelector('.collection'),
-        gallery: document.querySelector('.gallery'),
-        galleryContainer: document.querySelector('.gallery > .container'),
-    };
-}
