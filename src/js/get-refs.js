620176bad8a2dcf76c1f02054e51086c8be0f910
KuzkinaOlga <o.kuzkina@gmail.com>
2022-06-15 19:28:46 +0200

    featur/footer

--- src/js/get-refs.js
+++ src/js/get-refs.js
@@ -1,28 +1,26 @@
 export function getRefs() {
-    return {
-        // containerMovies: document.querySelector('.collection'),
-        gallery: document.querySelector('.gallery'),
-        linkCard: document.querySelectorAll('.post'),
-        // galleryContainer: document.querySelector('.gallery>.container'),
+  return {
+    // containerMovies: document.querySelector('.collection'),
+    gallery: document.querySelector('.gallery'),
+    linkCard: document.querySelectorAll('.post'),
+    // galleryContainer: document.querySelector('.gallery>.container'),
 
-        // Header Refs
-        headerContainer: document.querySelector('.js-header-container'),
-        nav: document.querySelector('.nav'),
-        logo: document.querySelector('.js-logo'),
-        homeBtn: document.querySelector('a[data-link="home"]'),
-        myLibraryBtn: document.querySelector('a[data-link="library"]'),
-        form: document.querySelector('.js-submit'),
-        myLibraryBtns: document.querySelector('.js-librari-list'),
-        watchedBtn: document.querySelector('.js-watched-btn'),
-        queueBtn: document.querySelector('.js-queue-btn'),
-        // Pagination-Tui
-        pagination: document.querySelector('.tui-pagination'),
+    // Header Refs
+    headerContainer: document.querySelector('.js-header-container'),
+    nav: document.querySelector('.nav'),
+    logo: document.querySelector('.js-logo'),
+    homeBtn: document.querySelector('a[data-link="home"]'),
+    myLibraryBtn: document.querySelector('a[data-link="library"]'),
+    form: document.querySelector('.js-submit'),
+    myLibraryBtns: document.querySelector('.js-librari-list'),
+    watchedBtn: document.querySelector('.js-watched-btn'),
+    queueBtn: document.querySelector('.js-queue-btn'),
+    // Pagination-Tui
+    pagination: document.querySelector('.tui-pagination'),
 
-        upBtn: document.querySelector('.up-btn'),
+    upBtn: document.querySelector('.up-btn'),
 
-        // Footer
-        footerLink: document.querySelector('.footer__modal')
-
-
-    };
+    // Footer
+    footerLink: document.querySelector('.footer__link'),
+  };
 }
