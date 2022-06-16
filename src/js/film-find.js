1b5316d298f5e926abce4720b39126b838901d4e
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-06-16 20:36:57 +0300

    1

--- src/js/film-find.js
+++ src/js/film-find.js
@@ -1,2 +1,39 @@
-// // import axios from 'axios';
+// import axios from 'axios';
+import { getRefs } from './get-refs';
+const searchFilm = getRefs().searchMovie;
+const galleryList = getRefs().gallery;
+const incorrectInput = getRefs().incorrectInput;
+// let gengesArray = [];
+import { renderList } from './render-list';
+import ApiService from './api';
+const getFilm = new ApiService();
+export let searchBy = '';
+export let queryForTui = '';
+
+//  onFormSubmit
+searchFilm.addEventListener('submit', onFormSubmit);
+function onFormSubmit(e) {
+  e.preventDefault();
+  getFilm.value = e.currentTarget.elements.searchQuery.value.trim();
+  queryForTui = getFilm.value;
+
+  if (!getFilm.value) {
+    return incorrectInput.classList.remove('is-hidden');
+  }
+
+
+  getFilm.getSearchMovies(getFilm.value).then(({ results }) => {
+    console.log(results.length);
+    if (results.length > 1) {
+      console.log(results.length);
+      incorrectInput.classList.add('is-hidden');
+    } else {
+      return incorrectInput.classList.remove('is-hidden');
+    }
+
+
+    galleryList.innerHTML = '';
+    return renderList(results, galleryList);
+  });
+}
 
