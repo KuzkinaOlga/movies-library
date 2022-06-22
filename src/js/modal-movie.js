ef0d01f6815ddae0382125a6cbb11d3b9d972f26
HryhoriiAndriiets1974 <ander_1_agg@ukr.net>
2022-06-22 19:34:11 +0300

    +trailer

--- src/js/modal-movie.js
+++ src/js/modal-movie.js
@@ -1,9 +1,11 @@
-
 import * as basicLightbox from 'basiclightbox'
 import { getRefs } from './get-refs';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import ApiService from './api';
 import noImg from '../images/no-poster-available.jpeg';
+import { Loading } from 'notiflix';
+import axios from 'axios';
+import 'basiclightbox/dist/basicLightbox.min.css';
 // Firebase import
 import { addFilmToFirebase } from './user-data'
 
@@ -38,11 +40,11 @@ export function onContainerClick(event) {
   const currentMovie = basicLightbox.create(`
     <div class="current-movie">
         <button data-modal-close class="modal__close-button-cm">
-        
+
         </button>
 
         <img  src="${posterPath(poster)}" class="current-movie__img">
-        
+
         <div class="current-movie__info">
 
         <h2 class="current-movie__title"> ${event.target.alt}</h2>
@@ -53,7 +55,7 @@ export function onContainerClick(event) {
         <li class="current-movie__name"> Original Title </li>
        <li class="current-movie__name"> Genre</li>
           </ul>
-        
+
         <ul class="info__value">
       <li class="info__item"><p > <span class="current-movie__vote-data">${vote} </span><span class="current-movie__votes_slash">/</span> <span class="current-movie__votes-data">${votes}</span></p></li>
       <li class="info__item"><p class="current-movie__popularity-data">${populanty}</p></li>
@@ -68,12 +70,13 @@ export function onContainerClick(event) {
         <div class="current-movie__btn-container">
         <button type = "button" class="current-movie_btn-add-to-watched btn">ADD TO WATCHED</button>
         <button type = "button" class="current-movie_btn-add-to-queue btn">ADD TO QUEUE</button>
+        <button type = "button" class="current-movie_btn-add-to-queue btn trailer-btn">Trailer</button>
       </div>
     </div>`
       );
 
     currentMovie.show();
-   
+
     const btnModalClose = document.querySelector(".modal__close-button-cm");
   const btnAddToWatched = document.querySelector(".current-movie_btn-add-to-watched");
   const btnAddToQueue = document.querySelector(".current-movie_btn-add-to-queue");
@@ -122,15 +125,28 @@ btnModalClose.addEventListener('click', ()=>{ currentMovie.close()});
         dataQueueMovie.push(currentMovieInfo);
 
         localStorage.setItem('queue', JSON.stringify(dataQueueMovie));
-        
+
         localStorage.setItem(ADD_TO_QUEUE_FILM, JSON.stringify(dataQueueMovie));
         // Firebase code
         filmType = 'queue';
         addFilmToFirebase(filmType, currentMovieInfo);
-        
+
       }));
 
-    }); 
+        // ========================= trailer
+      const trailerBtn = document.querySelector('.trailer-btn');
+
+      trailerBtn.addEventListener('click', event => {
+        event.preventDefault();
+        getTrailer(id);
+      });
+
+      window.addEventListener('keydown', e => {
+        const trailerRef = document.querySelector('.basicLightbox');
+        if (e.code === 'Escape' && !trailerRef) closeModal();
+      });
+
+    });
 
   function onImageClose(event) {
     if (event.code === 'Escape') {
@@ -138,6 +154,31 @@ btnModalClose.addEventListener('click', ()=>{ currentMovie.close()});
       window.removeEventListener('keydown', onImageClose);
       btnModalClose.removeEventListener('click', ()=>{ currentMovie.close();});
     }
+  }
+  // ========================= trailer
+
+  async function getTrailer(n) {
+    try {
+      Loading.arrows({
+        svgColor: '#ff6b08',
+        backgroundColor: 'rgba(0,0,0,0.25)',
+        cssAnimation: true,
+        cssAnimationDuration: 2000,
+        clickToClose: true,
+      });
+      const response = await axios.get(`https://api.themoviedb.org/3/movie/${n}/videos?&api_key=419c8d7d79cbcac22c5520f1ac14d2c7`);
+      const trailer = await response.data.results[0].key;
+
+      const trailerShow = basicLightbox.create(
+        `<iframe width="1024" height="630" style="border: none;" src='https://www.youtube.com/embed/${trailer}?autoplay=1&mute=1&controls=1' allow="fullscreen"></iframe>`,
+      );
+      trailerShow.show();
+      window.addEventListener('keydown', e => {
+        if (e.key === 'Escape' && trailerShow.visible()) trailerShow.close();
+      });
+      Loading.remove();
+    } catch (error) {
+      console.error(error);
   }
 }
 
@@ -147,5 +188,4 @@ function posterPath(poster) {
     return noImg;
   }
   return `https://image.tmdb.org/t/p/w500${poster}`;
-}
-
+};}
